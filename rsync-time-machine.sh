3102d6dc06379111188f22d115a95c417c1ee09f
Sam Doran <github@samdoran.com>
2013-04-09 10:59:43 -0400

    Initial commit

---
+++ rsync-time-machine.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+#
+# Backup mimicking Time Machine from Mac OS X using rsync
+
+
+
+# --- Variables --- #
+
+OS=$(uname -s)
+HOST=$(hostname)
+DATE_FORMAT=$(date "+%Y-%m-%d-%H%M%S")
+CURRENT_YEAR=$(date +%Y)
+CURRENT_MONTH=$(date +%m)
+
+# Use absolute paths. Relative paths tend to break the hard linking advantage of rsync.
+# Paths can include spaces as long as variable contents are double quoted
+SOURCE="[absolute path to source directory]"
+DESTINATION="[absolute path to backup destination]/$HOST"
+
+# Each exclude string must be separated by a space
+# Make sure the EXCLUDE variable contents are enclosed in double quotes
+# Example: " .* .~* *somestring*.jpg "
+EXCLUDE=" "
+
+
+
+# --- Main Program --- #
+
+# Create destination if it does not exist
+if [[ ! -d "$DESTINATION" ]] ; then
+  mkdir -p "$DESTINATION"
+fi
+
+# Create exclude string
+for i in $EXCLUDE ; do
+  EXCLUDESTRING="$EXCLUDESTRING --exclude "$i" "
+done
+
+# Make inital backup if Latest does not exist, otherwise only copy what has changed
+# and hard link to files that are the same
+if [[ ! -L "$DESTINATION"/Latest ]] ; then
+  rsync -aPh \
+                --delete \
+                $EXCLUDESTRING \
+                "$SOURCE" "$DESTINATION"/$DATE_FORMAT
+else
+  rsync -aPh \
+               --delete \
+               $EXCLUDESTRING \
+               --link-dest="$DESTINATION"/Latest \
+               "$SOURCE" "$DESTINATION"/$DATE_FORMAT
+fi
+
+# Remove symlink to previous Latest backup
+rm -f "$DESTINATION"/Latest
+
+# Create symlink to latest backup
+ln -s $DATE_FORMAT "$DESTINATION"/Latest
+
+
+
+# --- Remove old backups --- #
+
+# BSD date in OS X has a different syntax than GNU date in Linux
+if [[ $OS == "Darwin" ]]; then
+
+    # Return YYYY-MM one year and one month ago from today
+    LAST_YEAR=$(date -v -1y "+%Y")
+    LAST_MONTH=$(date -v -1m "+%m")
+    REMOVE_YEAR_MONTH=$(date -v -1y -v -1m  "+%Y-%m")
+
+elif [[ $OS == "Linux" ]]; then
+
+    # Return YYYY-MM one year and one month ago from today
+    LAST_YEAR=$(date -d "last year" "+%Y")
+    LAST_MONTH=$(date -d "last month" "+%m")
+    REMOVE_YEAR_MONTH=$LAST_YEAR-$LAST_MONTH     
+fi
+
+
+# Keep monthly backups for one year
+for (( month = 1 ; month < $CURRENT_MONTH ; month++ )); do
+  # List latest backup from each month of current year
+  # Use printf to pad the single digit months with a 0
+  LATEST_BACKUP=$(find "$DESTINATION" -mindepth 1 -maxdepth 1 -name ${CURRENT_YEAR}-$(printf "%02d" $month)-* | sort | tail -n 1)
+  find "$DESTINATION" -mindepth 1 -maxdepth 1 -name ${CURRENT_YEAR}-$(printf "%02d" $month)-* | grep -v "$LATEST_BACKUP" | xargs -I {} rm -rf {}
+done
+
+for (( month = $CURRENT_MONTH ; month <= 12 ; month++ )); do
+  # List latest backup from each month of current year
+  # Use printf to pad the single digit months with a 0
+  LATEST_BACKUP=$(find "$DESTINATION" -mindepth 1 -maxdepth 1 -name ${LAST_YEAR}-$(printf "%02d" $month)-* | sort | tail -n 1)
+  find "$DESTINATION" -mindepth 1 -maxdepth 1 -name ${LAST_YEAR}-$(printf "%02d" $month)-* | grep -v "$LATEST_BACKUP" | xargs -I {} rm -rf {}
+done
+
+
+# Remove backups older than one year
+for (( month = 1 ; month < $CURRENT_MONTH ; month++ )); do
+  find "$DESTINATION" -mindepth 1 -maxdepth 1 -type d -name "$LAST_YEAR-$(printf "%02d" $month)-*" | xargs -I {} rm -rf {}
+done
+
+find "$DESTINATION" -mindepth 1 -maxdepth 1 -type d ! -name "$CURRENT_YEAR-*" | grep -v "$LAST_YEAR-*" | xargs -I {} rm -rf {}
